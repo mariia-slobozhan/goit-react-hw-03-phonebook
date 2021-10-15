d6a5a7e9339bc20565c08cf43c6b918f3315682e
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-10-15 19:26:08 +0300

    add localeStorage logic

--- src/App.js
+++ src/App.js
@@ -16,6 +16,20 @@ export default class App extends Component {
     filter: "",
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addNewContact = (contact) => {
     this.setState((prevState) => {
       const normalizedContact = contact.name.toLowerCase();
